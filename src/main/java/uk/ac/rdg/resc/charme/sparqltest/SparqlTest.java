b08efe098e288b6bae2d4c8251af347d1579b00e
raquelalegre <raquel.alegre@gmail.com>
2014-11-18 11:40:54 +0000

    firs

---
+++ src/main/java/uk/ac/rdg/resc/charme/sparqltest/SparqlTest.java
@@ -0,0 +1,42 @@
+package uk.ac.rdg.resc.charme.sparqltest;
+
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.Syntax;
+import com.hp.hpl.jena.sparql.engine.QueryEngineFactory;
+import com.hp.hpl.jena.sparql.engine.http.HttpQuery;
+
+public class SparqlTest {
+
+	public static void main(String[] args) {
+		//String queryString = "SELECT DISTINCT ?a ?b ?c WHERE {  ?a ?b ?c .} LIMIT 25 OFFSET 0";
+		//String service = "http://test.strabon.di.uoa.gr/NOA/Query";
+		//String service = "http://linkedgeodata.org/sparql";
+		String queryString = "SELECT ?s { ?s ?p ?o . } LIMIT 10";
+		String service = "http://test.strabon.di.uoa.gr/NOA/Query"; 
+		Query query = QueryFactory.create(queryString);
+		query.setSyntax(Syntax.syntaxARQ);
+		HttpQuery q = new HttpQuery(service);
+		q.setForcePOST();
+		QueryEngineFactory
+		
+		
+		QueryExecution qe = QueryExecutionFactory.sparqlService(service, query);
+		
+		
+		
+		ResultSet results = qe.execSelect();
+		
+		while(results.hasNext()) {
+			QuerySolution solution = results.nextSolution();
+			System.out.println(solution.toString());
+		}
+		
+	}
+
+}
+
